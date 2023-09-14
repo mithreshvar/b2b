d7db152c1e0c0d4aeeb6b735fe0e29314241f252
superb-shan <shankar.k2021csec@sece.ac.in>
2023-09-14 22:45:51 +0530

    FundsIndia logo added in login page

--- src/app/components/register-with-us/NavBar.js
+++ src/app/components/register-with-us/NavBar.js
@@ -2,20 +2,15 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-// import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{height: '60px', backgroundColor: "white", px: '80px', boxShadow: '0px 3px 6px #0000001A'}}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, color: "primary.main" }}>
-            <Link href={'/'}>FundsIndia</Link>
-          </Typography>
+      <AppBar position="static" sx={{height: '60px', backgroundColor: "white", px: '70px', boxShadow: '0px 3px 6px #0000001A'}}>
+        <Toolbar sx={{display: "flex", justifyContent: "space-between"}}>
+          <Link href={'/'}><Image src='/logo.svg' width={125} height={36} /></Link>
           <div className='font-poppins text-primary font-semibold'>Contact Us</div>
         </Toolbar>
       </AppBar>
