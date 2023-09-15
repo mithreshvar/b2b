4bf06d2fbd7a4e2d7bd9aad3212067bb48ddde60
MaverickShroov <83708479+MaverickShroov@users.noreply.github.com>
2023-09-15 19:29:06 +0530

    routes update

---
+++ src/app/components/partner/Dashboard/index.js
@@ -0,0 +1,9 @@
+function Dashboard() {
+    return (
+        <p className="w-full bg-white">
+            Dashboard
+        </p>
+    );
+}
+
+export default Dashboard;
--- src/app/partner/page.js
+++ src/app/partner/page.js
@@ -10,19 +10,20 @@ import Image from 'next/image';
 import { ThemeProvider } from '@mui/material';
 import theme from '../theme';
 
-import Dashboard from '/public/partner/Dashboard.js'
-import About from '/public/partner/About.js'
-import Brokerage from '/public/partner/Brokerage.js'
-import Dbf from '/public/partner/Dbf.js'
-import Home from '/public/partner/Home.js'
-import Manual from '/public/partner/Manual.js'
-import Password from '/public/partner/Password.js'
-import Portfolio from '/public/partner/Portfolio';
-import Registration from '/public/partner/Registration';
-import Reports from '/public/partner/Reports';
-import Tripartite from '/public/partner/Tripartite';
+import IDashboard from '/public/partner/Dashboard.js'
+import IAbout from '/public/partner/About.js'
+import IBrokerage from '/public/partner/Brokerage.js'
+import IDbf from '/public/partner/Dbf.js'
+import IHome from '/public/partner/Home.js'
+import IManual from '/public/partner/Manual.js'
+import IPassword from '/public/partner/Password.js'
+import IPortfolio from '/public/partner/Portfolio';
+import IRegistration from '/public/partner/Registration';
+import IReports from '/public/partner/Reports';
+import ITripartite from '/public/partner/Tripartite';
 
 import back from '/public/partner/back.svg'
+import Dashboard from '../components/partner/Dashboard';
 
 
 export default function NavBar() {
@@ -50,76 +51,88 @@ export default function NavBar() {
 
                     <div className='flex gap-x-[14px] items-center relative'>
                         {(active==='dashboard') && <div className='absolute w-[3px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('dashboard')}}><Dashboard active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('dashboard')}}><IDashboard active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='dashboard' && 'font-semibold text-primary'}`} onClick={()=>{setActive('dashboard')}} >Dashboard</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'home' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('home')}}><Home active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('home')}}><IHome active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='home' && 'font-semibold text-primary'}`} onClick={()=>{setActive('home')}} >Partner Home</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'registration' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('registration')}}><Registration active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('registration')}}><IRegistration active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='registration' && 'font-semibold text-primary'}`} onClick={()=>{setActive('registration')}} >New User Registration</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'tripartite' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('tripartite')}}><Tripartite active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('tripartite')}}><ITripartite active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='tripartite' && 'font-semibold text-primary'}`} onClick={()=>{setActive('tripartite')}} >Tripartite Agreement</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'portfolio' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('portfolio')}}><Portfolio active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('portfolio')}}><IPortfolio active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='portfolio' && 'font-semibold text-primary'}`} onClick={()=>{setActive('portfolio')}} >Partner Portfolio</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'reports' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('reports')}}><Reports active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('reports')}}><IReports active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='reports' && 'font-semibold text-primary'}`} onClick={()=>{setActive('reports')}} >Reports</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'brokerage' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('brokerage')}}><Brokerage active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('brokerage')}}><IBrokerage active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='brokerage' && 'font-semibold text-primary'}`} onClick={()=>{setActive('brokerage')}} >Brokerage Details</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'dbf' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('dbf')}}><Dbf active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('dbf')}}><IDbf active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='dbf' && 'font-semibold text-primary'}`} onClick={()=>{setActive('dbf')}} >DBF File Download</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'about' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('about')}}><About active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('about')}}><IAbout active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='about' && 'font-semibold text-primary'}`} onClick={()=>{setActive('about')}} >About Partner</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'manual' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('manual')}}><Manual active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('manual')}}><IManual active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='manual' && 'font-semibold text-primary'}`} onClick={()=>{setActive('manual')}} >Partner Manual</h6>
                     </div>
 
                     <div className='flex gap-x-[14px] items-center relative '>
                         {active === 'password' && <div className='absolute w-[4px] h-[30px] rounded-r-[2px] bg-primary ml-[-19px]' />}
-                        <span className='cursor-pointer' onClick={()=>{setActive('password')}}><Password active={active} /></span>
+                        <span className='cursor-pointer' onClick={()=>{setActive('password')}}><IPassword active={active} /></span>
                         <h6 className={`cursor-pointer ${active==='password' && 'font-semibold text-primary'}`} onClick={()=>{setActive('password')}} >Change Password</h6>
                     </div>
 
                 </div>
 
                 {/* page segment */}
-                <div className='bg-[#F5F7FE] h-full w-[calc(100vw-250px)] z-0'>
+                <div className='bg-[#F5F7FE] h-full w-[calc(100vw-250px)] z-0 p-[20px] pr-0 '>
 
-
+                    {
+                        (active==='about' && <Dashboard /> )||
+                        (active==='brokerage' && <Dashboard /> )||
+                        (active==='dashboard' && <Dashboard /> )||
+                        (active==='dbf' && <Dashboard /> )||
+                        (active==='home' && <Dashboard /> )||
+                        (active==='manual' && <Dashboard /> )||
+                        (active==='password' && <Dashboard /> )||
+                        (active==='portfolio' && <Dashboard /> )||
+                        (active==='registration' && <Dashboard /> )||
+                        (active==='reports' && <Dashboard /> )||
+                        (active==='tripartite' && <Dashboard /> )
+                    }
 
                 </div>
 
