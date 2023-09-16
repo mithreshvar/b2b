9f140444236539e22ba508488097c4bd28da5c7f
superb-shan <shankar.k2021csec@sece.ac.in>
2023-09-16 23:45:17 +0530

    error cases of change passowrd

--- src/app/components/partner/Password/index.js
+++ src/app/components/partner/Password/index.js
@@ -1,31 +1,70 @@
 import { useState } from "react";
 import { CustomPasswordField, CustomTextField } from "../../InputFields";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Password = () => {
 
+    const router = useRouter();
+
     const [currentPassword, setCurrentPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
-    // const [currentPassword, setCurrentPassword] = useState('');
-    const [confirmPassword,setConfirmPassword]=useState('')
+    const [confirmPassword,setConfirmPassword]=useState('');
+
+    const [currentPasswordErrorMessage, setCurrentPasswordErrorMessage] = useState('');
+    const [newPasswordErrorMessage, setNewPasswordErrorMessage] = useState('');
+    const [confirmPasswordErrorMessage , setConfirmPasswordErrorMessage ]= useState('');
+
+    const handlePasswordErrorCheck = (value, setPasswordErrorMessage) =>{
+        
+        if (value === "") {
+            setPasswordErrorMessage("Password cannot be empty");
+            return false;
+          } else if (value.length < 8) {
+            setPasswordErrorMessage("Password must be at least 8 characters long");
+            return false;
+          } else {
+            setPasswordErrorMessage("");
+            return true;
+        }
+
+    }
 
     const handleCurrentPasswordChange = (event) => {
         const value = event.target.value;
         setCurrentPassword(value);
+        handlePasswordErrorCheck(value, setCurrentPasswordErrorMessage);        
     }
 
     const handleNewPasswordChange = (event) => {
         const value = event.target.value;
         setNewPassword(value);
+        handlePasswordErrorCheck(value, setNewPasswordErrorMessage);
     }
 
     const handleConfirmPasswordChange = (event) => {
         const value = event.target.value;
         setConfirmPassword(value);
+        handlePasswordErrorCheck(value, setConfirmPasswordErrorMessage);
+        if (value !== newPassword ){
+            setConfirmPasswordErrorMessage("New password & Confirm password doesn't match");
+        }
     }
 
     const handleSubmit = () => {
 
+        const cuPass = handlePasswordErrorCheck(currentPassword, setCurrentPasswordErrorMessage);
+        const nePass = handlePasswordErrorCheck(newPassword, setNewPasswordErrorMessage);
+        const coPass = handlePasswordErrorCheck(confirmPassword, setConfirmPasswordErrorMessage);
+
+        if ( cuPass && nePass && coPass ){
+            if (newPassword !== confirmPassword){
+                setConfirmPasswordErrorMessage("New password & Confirm password doesn't match");
+            }
+            else{
+                router.push("/?modal=password-updated");
+            }
+        }
     }
 
     const handleBack = () => {
@@ -41,12 +80,12 @@ const Password = () => {
                     <span className="ml-[5px] font-bold">testadmin@gmail.com</span>
                 </div>
 
-                <CustomTextField label="Current Password" value={currentPassword} handleChange={handleCurrentPasswordChange} />
-                <CustomPasswordField label="New Password (Minimum 8 Characters)" value={newPassword} handleChange={handleNewPasswordChange} />
-                <CustomPasswordField label="Confirm Password" value={confirmPassword} handleChange={handleConfirmPasswordChange} />
+                <CustomTextField label="Current Password" value={currentPassword} handleChange={handleCurrentPasswordChange} errorMessage={currentPasswordErrorMessage} />
+                <CustomPasswordField label="New Password (Minimum 8 Characters)" value={newPassword} handleChange={handleNewPasswordChange} errorMessage={newPasswordErrorMessage} />
+                <CustomPasswordField label="Confirm Password" value={confirmPassword} handleChange={handleConfirmPasswordChange} errorMessage={confirmPasswordErrorMessage} />
 
                 <div className="flex gap-[20px]">
-                    <Link href="/?modal=password-updated" ><button className="w-[108px] h-[40px] bg-[#0071E7] rounded-[25px] text-white font-semibold flex justify-center items-center" onClick={handleSubmit}>Submit</button></Link>
+                    <button className="w-[108px] h-[40px] bg-[#0071E7] rounded-[25px] text-white font-semibold flex justify-center items-center" onClick={handleSubmit}>Submit</button>
                     <button className="w-[108px] h-[40px] bg-white border-[1px] border-[#0071E7] rounded-[25px] text-[#0071E7] font-semibold flex justify-center items-center" onClick={handleBack}>Back</button>
                 </div>
             </div>
