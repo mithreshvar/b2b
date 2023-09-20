5552ffab3f2f8e434da7a68f72664efb2b6e7d8f
Karthikeyan-amk <karthikeyananbarasu7@gmail.com>
2023-09-20 20:37:53 +0530

    Coming soon updated

--- src/app/components/partner/Report/index.js
+++ src/app/components/partner/Report/index.js
@@ -1,26 +1,18 @@
 import Image from "next/image"
-// import IReport from '../../../../../public/partner/'
+import IReport from 'public/partner/shutterstock_2130751070 [Converted]/shutterstock_2130751070 [Converted]@2x.png'
 
 function report() {
   return (
     <div className="p-[20px]" >
       <div className="w-full h-[469px] bg-white p-[20px] rounded-[20px]  ">
-
         <div className="text-[20px] font-semibold leading-[20px]">Reports</div>
-
-        <div className="flex flex-col h-[259px] items-center">
-          <div className="w-[169px] h-[177px]">
-            {/* <Image src={IReport}/> */}
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="w-[169px]">
+            <Image src={IReport}/>
           </div>
-          <div className="text-[18px] leading-[18px] mt-[30px] mb-[20px]">
-          Coming Soon-Reports
-          </div>
-          <div className="text-[14px]  leading-[14px] text-[#6E6E72] ">
-          Stay tuned for updates.
-          </div>
-
+          <div className="text-[18px] leading-[18px] mt-[30px] mb-[20px]">Coming Soon-Reports</div>
+          <div className="text-[14px]  leading-[14px] text-[#6E6E72] ">Stay tuned for updates.</div>
         </div>
-
       </div>
     </div>
   )
