b4a65f825d2ace2f163ab494664a7aadf91b3150
MaverickShroov <83708479+MaverickShroov@users.noreply.github.com>
2023-09-22 23:09:23 +0530

    customized table rows

--- src/app/components/partner/PartnerHome/Table.js
+++ src/app/components/partner/PartnerHome/Table.js
@@ -1,12 +1,21 @@
 import TableRow from "./TableRow";
 
 export default function Table({headers, tenData=[], setActive}) {
+
+    let type, schemeData;
+    if(headers[3]=='Systematic Plans') type = 'sip';
+    if(headers[0]=='Portfolio Name'){
+        type = 'portfolio';
+    }
+    if(headers[2]=='Rating') type = 'rating';
+    if(headers[1]=='Scheme Name') type = 'schemes';    
+
     return(
         <div className=" flex flex-col bg-white p-[20px] rounded-[15px] text-left ">
             {
                 (tenData.length === 0) ?
                 <h1 className="text-[24px] font-bold text-center">
-                    No User Found !
+                    No Data Found !
                 </h1>
                 :
                 <table cellPadding={'15px'} >
@@ -14,8 +23,12 @@ export default function Table({headers, tenData=[], setActive}) {
                         {headers.map( ele =><th className={`pb-[20px] pt-[5px] font-medium `}>{ele}</th>)}
                     </tr>
                     {
-                        tenData.map( ele =>{
-                            return <TableRow data={ele} setActive={setActive} sip={(headers[3]=='Systematic Plans')?true:false} portfolio={(headers[0]=='Portfolio Name')?true:false} />
+                        tenData.map( (ele, index) =>{
+                            if (type=='schemes' && ele[0] != index+1){
+                                if (typeof ele[0] == 'number') ele.shift()
+                                ele.unshift(index+1);
+                            }
+                            return <TableRow data={ele} setActive={setActive} type={type} />
                         })
                     }
                 </table>
