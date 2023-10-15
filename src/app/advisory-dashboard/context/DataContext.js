12cc819eefb17ccb2e21ab3b65b53ed030624c72
MaverickShroov <83708479+MaverickShroov@users.noreply.github.com>
2023-10-15 12:20:19 +0530

    navbar control added

--- src/app/advisory-dashboard/context/DataContext.js
+++ src/app/advisory-dashboard/context/DataContext.js
@@ -2,10 +2,10 @@ import { createContext, useContext, useEffect, useState } from "react";
 const DataContext = createContext();
 
 export function DataContextProvider(props) {
-
+    const [navOpen, setNavOpen] = useState(true);
 
     return(
-        <DataContext.Provider value={{}}>
+        <DataContext.Provider value={{navOpen, setNavOpen}}>
             {props.children}
         </DataContext.Provider>
     );
