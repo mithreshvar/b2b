1ffd37f56f96a363c529cfe07a93c887a1ae7d4f
ragunandhant <raja.g2020cse@sece.ac.in>
2023-10-16 20:45:28 +0530

    changes for bug in note

--- src/app/advisory-dashboard/components/Portfolio/AddNote.js
+++ src/app/advisory-dashboard/components/Portfolio/AddNote.js
@@ -19,6 +19,7 @@ function AddNote() {
             "Notes": "Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator."
         },
     ])
+    const [Delete, setDelete] = useState(false)
     const [Index, setIndex] = useState(0)
     const [text, settext] = useState("Notes")
     const [edit, setEdit] = useState(false)
@@ -38,9 +39,17 @@ function AddNote() {
         setEdit(false)
         setIsfocus(false)
     }
+const DeleteNote = ()=>{
+
+      const data = Description
+      data.splice(Index,1)
+      setDescription(data)
+      setDelete(false)
+}
 
     return (
-        <div className='bg-white w-[100%] h-[610px] pr-[15px] pl-[30px]'>
+        <div>
+        <div className={` w-[100%] h-[479px] pr-[15px] pl-[30px] ${Delete ? "opacity-30 ":"opacity-100 bg-white "}`}>
 
             <div className='w-full flex justify-end'>
                 <Image src={close} className='mt-[15px]' />
@@ -51,7 +60,7 @@ function AddNote() {
 
 
             {focus ? (<legend className={`absolute mt-[13.3px] backdrop-blur-3xl bg-white ml-[20px] text-[12px] w-[42px] pl-[5px] text-[#6E6E72]`}>Note</legend>) : ""}
-            <textarea className={`w-full mr-[15px] mt-[20px] border-[#E4E5E5] text- ${edit || text !== "Notes" ? "[#6E6E72]" : "black   "} border-[2px] pt-[5px] pl-[15px] rounded-[8px]`} value={text} onChange={textchange} onClick={() => { setIsfocus(true), settext("") }} onBlur={() => { setIsfocus(false); edit ? "" : settext("Note"); }}></textarea>
+            <textarea className={`w-full mr-[15px] mt-[20px] border-[#E4E5E5] text- ${edit || text !== "Notes" ? "[#6E6E72]" : "black   "} border-[2px] pt-[5px] pl-[15px] rounded-[8px]`} value={text} onChange={textchange} onClick={() => { setIsfocus(true), settext("") }} onBlur={() => { setIsfocus(false); edit ||  text !== "Note"? "" : settext("Note"); }}></textarea>
 
 
 
@@ -87,7 +96,7 @@ function AddNote() {
                                             <td className="p-4 text-left text-[14px] w-[63%] h-[32px]">{ele.Notes}</td>
                                             <div className='flex gap-[30px]'>
                                                 <td className="p-4 text-left flex items-center text-[#0066CD] cursor-pointer font-medium h-[12px] w-[50px] gap-[3px]" onClick={() => { setIndex(ind); setEdit(true); settext(ele.Notes); setIsfocus(true) }}> <Image src={editIcon} className='h-[12px]' /> Edit</td>
-                                                <td className="p-4 text-left flex items-center text-[#F56902] cursor-pointer font-medium h-[12px] w-[50px] ml-[20px] pl-[20px] gap-[3px]"> <Image src={deleteIcon} className='h-[12px]' /> Delete</td>
+                                                <td className="p-4 text-left flex items-center text-[#F56902] cursor-pointer font-medium h-[12px] w-[50px] ml-[20px] pl-[20px] gap-[3px]" onClick={()=>{setIndex(ind);setDelete(true)}}> <Image src={deleteIcon} className='h-[12px]' /> Delete</td>
                                             </div>
                                         </tr>
                                     )
@@ -98,6 +107,20 @@ function AddNote() {
                     </table>
                 </div>
             </div>
+        </div>
+        {Delete ? <div className='w-[100%] h-[230px] absolute flex align-baseline bg-[#FDE4D5] border-[#FF7922] border-[2px] justify-center items-center' onBlur={()=>{setDelete(false)}}>
+            <div>
+            <p className='text-[#000000] font-bold text-[18px] text-center'>Delete Confirmation</p>
+            <br />
+            <p className='text-[#000000]  text-[14px]'>Are you sure, you want to delete the note created on {moment(Description[Index].Date, "DD-MM-YYYY").format("D MMM YYYY")}?</p>
+            <div className=' flex justify-center gap-[40px] mt-[41px]' >
+            <button className='bg-[#FF7922] rounded-[25px] text-bold text-white w-[150px] h-[50px] text-[20px]' onClick={()=>{setDelete(false)}}>CANCEL</button>
+            <button className='text-[#FF7922] rounded-[25px] bg-[#FFECE0] w-[150px] h-[50px] text-[20px] text-bold border-[#FF7922] border-[1px]' onClick={DeleteNote} >DELETE</button>
+          
+            </div>
+            </div>
+        </div>:""}
+        
         </div>
     )
 }
