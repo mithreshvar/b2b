8ca59efe6006944c9ef8354b65ec45235f62d1eb
ragunandhant <raja.g2020cse@sece.ac.in>
2023-10-18 15:56:32 +0530

    changes for filter array

--- src/app/advisory-dashboard/components/Portfolio/CustomDropSelectField.js
+++ src/app/advisory-dashboard/components/Portfolio/CustomDropSelectField.js
@@ -1,73 +1,81 @@
-import { Checkbox, FormControl, FormControlLabel, FormGroup, InputLabel, ListItemText, Menu, MenuItem, Select } from '@mui/material'
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { Checkbox, FormControlLabel } from '@mui/material';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import React, { useEffect, useState } from 'react'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
 
+export default function CustomDropSelectField(props) {
+    const array = new Array(props.value.length);
+    array.fill(true);
+    const [state, setState] = React.useState(array);
 
-
-function CustomDropSelectField() {
-const title = "Basic Details"
-const handleCheck = (event)=>{
-    const res = Data;
-
-    res[index].checked = event.target.checked
-    setData(res)
-    console.log(Data,index)
-}
-const [index,setindex] = useState()
-    const [Data, setData] = useState([
-
-        {
-            "name": "net Inflow YTD",
-            "value": "net Inflow YTD",
-            "checked": true
-
-        } ,   {
-            "name": "net Inflow YTD",
-            "value": "net Inflow YTD",
-            "checked": true
-
+    const handleChanges = (event, index) => {
+        if (event.target.checked) {
+            const updatedColumns = [...props.columns[event.target.name], props.value[index]];
+            const data = state.slice();
+            data[index] = true;
+            setState(data);
+            props.handleChange({ ...props.columns, [event.target.name]: updatedColumns });
+        } else {
+            const updatedColumns = props.columns[event.target.name].filter((ele) => ele !== props.value[index]);
+            const data = state.slice();
+            data[index] = false;
+            setState(data);
+            props.handleChange({ ...props.columns, [event.target.name]: updatedColumns });
         }
-    ])
-    useEffect(()=>{},[Data])
+    };
+
     return (
-        <div>
-            <FormControl sx={{ width: "100px" }}>
-
-
-                <InputLabel id="demo-simple-select-label" >{title}</InputLabel>
-                <Select labelId="demo-simple-select-label" label={title} sx={{ width: "350px", borderRadius: "12px", height: "40px", borderWidth: "1px", borderColor: "#E7E7E9" }} IconComponent={(props) => {
-
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="demo-select-small-label">{props.title}</InputLabel>
+            <Select
+                sx={{
+                    width: "300px",
+                    height: "40px",
+                    borderRadius: "10px"
+                }}
+                labelId="demo-select-small-label"
+                id="demo-select-small"
+                value=""
+                label="Age"
+                IconComponent={(props) => {
                     if (props.className.includes('MuiSelect-iconOpen'))
-                        return <KeyboardArrowDownIcon sx={{ color: "blue", marginRight: "15px" }} />
-
-                    return <KeyboardArrowUpIcon sx={{ color: "blue", marginRight: "15px" }} />
-                }} multiple value={[]} >
-
-                    <FormGroup>
-                    <FormControlLabel className='pl-[10px]' control={<Checkbox size='small' icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} value={"hello"} />} label="All" />
-                        {
-                            Data.map((ele,i) => {
-
-                               return  (<FormControlLabel className='pl-[10px]' control={<Checkbox size='small' icon={<RadioButtonUncheckedIcon />} checkedIcon={<CheckCircleIcon />} value={ele.value}  checked={ele.checked} onChange={(event)=>{setindex(i);handleCheck(event)}}/>} label={ele.name} />)
-
-
-                            })
+                        return <KeyboardArrowDownIcon sx={{ color: "#0171E7", marginRight: "15px" }}/>
+                    return <KeyboardArrowUpIcon sx={{ color: "#0171E7", marginRight: "15px" }}/>
+                }}
+                MenuProps={{
+                    sx: {},
+                    PaperProps: {
+                        sx: {
+                            backgroundColor: '#F8F8F8',
+                            borderRadius: '15px',
+                            boxShadow: '0px 8px 15px #00000026',
                         }
-                        
-                
-                    </FormGroup>
-
-                </Select>
-
-
-
-
-            </FormControl>
-        </div>
-    )
+                    }
+                }}
+            >
+                <FormGroup className='pl-[10px]'>
+                    {props.value.map((ele, index) => (
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    checked={state[index]}
+                                    onChange={(event) => handleChanges(event, index)}
+                                    name={props.title}
+                                    icon={<RadioButtonUncheckedIcon />}
+                                    checkedIcon={<CheckCircleIcon />}
+                                />
+                            }
+                            label={ele}
+                        />
+                    ))}
+                </FormGroup>
+            </Select>
+        </FormControl>
+    );
 }
-
-export default CustomDropSelectField    
