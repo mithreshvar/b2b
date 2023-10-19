6936362beafeb96295a97fcb2b15fee8a819da72
Jeethula <jeethula10@gmail.com>
2023-10-19 10:10:35 +0530

    grid

--- src/app/advisory-dashboard/components/Portfolio/Filter.js
+++ src/app/advisory-dashboard/components/Portfolio/Filter.js
@@ -144,27 +144,31 @@ function Filter() {
                 <div className={`w-[141px] h-[33px] text-center text-bold text-[14px] rounded-t-[10px] pt-[5px] cursor-pointer ${Filter === "second" ? "bg-[#DCEBFE] text-[#0071E7]" : "bg-[#F7F8FF] text-[#BEBEBE]"}`} onClick={() => { setFilter("second") }}>Filters 2</div>
             </div>
             {
-                Filter === "first" ?
-                    <div className='w-full bg-white  '>
-                        {
-                            Object.keys(columns).map((ele) => {
-
-
-                                return <CustomDropSelectField title={ele} value={columns[ele]} columns={unSelectedColumn} handleChange={handleColumnChange} />
-                            })
-                        }
+                Filter === "first" ?(
+                    <div className='w-[990px] bg-white  '>
+                        <div className='grid grid-cols-3 gap-[17px]'>
+                            {Object.keys(columns).map((ele) => (
+                            <CustomDropSelectField
+                                title={ele}
+                                value={columns[ele]}
+                                columns={unSelectedColumn}
+                                handleChange={handleColumnChange}
+                            />
+                            ))}
+                        </div>
                         <div className='pt-[50px] flex flex-row-reverse gap-[30px]'>
 
                             <div className='w-[108px] h-[40px] text-white font-semibold text-center text-[14px] pt-[10px] bg-[#0071E7] rounded-[20px] cursor-pointer' onClick={handleApplyFilter}>Apply Filter</div>
                             <div className='w-[108px] h-[40px] text-[#0071E7] flex gap-[5px] font-semibold justify-center align-middle text-[14px] pt-[10px] bg-white rounded-[20px] cursor-pointer' onClick={handleClearFilter} > <Image src={clearFilter} className='mb-[12px]' />Clear Filter</div>
                         </div>
-                    </div>
+                    </div>)
                     :
 
-
+                        (
                     <div className='w-full h-[100px] '>
                         <div>Filter2</div>
                     </div>
+                        )
             }
 
 
