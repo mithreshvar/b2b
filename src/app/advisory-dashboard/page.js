1ff1a13f2ead5d16a5cd362d0aed0e27a0c097f4
superb-shan <shankar.k2021csec@sece.ac.in>
2023-10-27 16:13:06 +0530

    Equ, Debt, Gold, 5Star

--- src/app/advisory-dashboard/page.js
+++ src/app/advisory-dashboard/page.js
@@ -1,5 +1,6 @@
 "use client"
 import Content from "./Content";
+import DefaultHoverPage, { DebtExposureHoverPage, FiveStarRatedHoverPage, GoldOthersExposureHoverPage, OvernightLiquidExposureHoverPage } from "./components/Portfolio/Hoverpage/HoverPage";
 import { DataContextProvider } from './context/DataContext';
 
 export default function  Home() {
@@ -7,7 +8,7 @@ export default function  Home() {
     return (
         
         <DataContextProvider>
-            <Content />
+            <FiveStarRatedHoverPage />
         </DataContextProvider>
 
     );
